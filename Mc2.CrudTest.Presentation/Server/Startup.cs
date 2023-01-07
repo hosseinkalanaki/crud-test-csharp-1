5298ae6bd545866f2bd8b0ace19e104092150552
hosseinkalanaki <46799257+hosseinkalanaki@users.noreply.github.com>
2023-01-07 03:43:58 +0330

    Create Backend , front end and database code first

--- Mc2.CrudTest.Presentation/Server/Startup.cs
+++ Mc2.CrudTest.Presentation/Server/Startup.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Mc2.CrudTest.Presentation.Server.Data;
 
 namespace Mc2.CrudTest.Presentation.Server
 {
@@ -22,6 +24,9 @@ namespace Mc2.CrudTest.Presentation.Server
 
             services.AddControllersWithViews();
             services.AddRazorPages();
+
+            services.AddDbContext<DatabaseContext>(item => item.UseSqlServer(Configuration.GetConnectionString("ConnectionString")));
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
